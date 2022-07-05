1733e9a629e6c3cdeb3db02aa0413088c6bf1ab2
Nguyễn Lam Sơn <67233512+lamsonkma@users.noreply.github.com>
2022-07-05 09:50:52 -0700

    fix: intl (#34)
    Co-authored-by: Nguyen Lam Son <sonnl.bhsoft@gmail.com>

--- constants/Money.ts
+++ constants/Money.ts
@@ -1,3 +1,5 @@
+import Intl from 'intl';
+import 'intl/locale-data/jsonp/en';
 const moneyFormatter = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' });
 
 export const moneyFormat = (money: number): string => moneyFormatter.format(money);
